6e5e56718a7c6c05f0a40f2c5a1410d639b6b500
Nick Johnstone <ncwjohnstone@gmail.com>
2017-12-28 19:40:15 +1300

    Put old example back in, turn strict mode back on

--- example/client.ts
+++ example/client.ts
@@ -1,86 +1,112 @@
-import {makeRemoteDataDriver, NotAsked} from '../src';
-import {makeDOMDriver, div, input, button} from '@cycle/dom';
-import {timeDriver} from '@cycle/time';
-import {run} from '@cycle/run';
+import {
+  makeRemoteDataDriver,
+  RemoteDataSource,
+  RemoteResponse,
+  NotAsked,
+  RemoteData
+} from '../src';
+import { makeDOMDriver, DOMSource, div, input, button, pre } from '@cycle/dom';
+import { timeDriver, TimeSource } from '@cycle/time';
+import { run } from '@cycle/run';
 import xs from 'xstream';
 
-function GithubSearch(sources) {
+interface Sources {
+  DOM: DOMSource;
+  Time: TimeSource;
+  RemoteData: RemoteDataSource;
+}
+
+interface Result {
+  name: string;
+  value: string;
+}
+
+function GithubSearch(sources: Sources) {
   const query$ = sources.DOM
-    .select('.search')
+    .select('.search-query')
     .events('input')
-    .map(ev => ev.target.value)
+    .map(ev => (ev.target as HTMLInputElement).value)
     .remember();
 
-  const reload$ = sources.DOM
-    .select('.reload')
-    .events('click');
+  const finishedTyping$ = query$.compose(sources.Time.debounce(250));
 
-  const search$ = xs.merge(
-    query$.compose(sources.Time.debounce(250)),
-    reload$
-  );
+  const searchClick$ = sources.DOM.select('.search').events('click');
 
-  const searchWithQuery$ = search$.map(() => query$.take(1)).flatten();
+  const reload$ = sources.DOM.select('.reload').events('click');
 
-  const post$ = searchWithQuery$
-    .map(query =>
-      query === ''
-        ? NotAsked
-        : sources.RemoteData.request({
-            url: `https://api.github.com/search/repositories?q=${query}`,
-            method: 'GET'
-          })
+  const search$ = xs.merge(finishedTyping$, reload$, searchClick$);
+
+  const data$ = search$.map(() => query$.take(1)).flatten();
+
+  const loadingProgress$ = sources.Time.periodic(300).map(i => i % 3 + 1);
+
+  const remoteData$ = data$
+    .map(query => {
+      if (query === '') {
+        return xs.of(NotAsked).remember();
+      }
+
+      return sources.RemoteData
+        .request({ url: `/?${query}`, method: 'GET' })
+        .debug('hi');
+    })
+    .flatten();
+
+  const post$ = remoteData$
+    .map((remoteData: RemoteResponse) =>
+      remoteData.rmap(res => res.body as Result[])
     )
-    .flatten()
-    .map(remoteData => remoteData.rmap(response => response.body))
     .startWith(NotAsked);
 
   return {
-    DOM: post$.map(view)
-  }
+    DOM: xs.combine(post$, loadingProgress$).map(view)
+  };
 }
 
-function view(remotePost) {
+function view([remotePost, loadingProgress]: [RemoteData<Result[]>, number]) {
   return div([
-    'Search github',
-    input('.search'),
+    div('Search github'),
+    input('.search-query'),
+    button('.search', 'Search'),
 
     remotePost.when({
-      Loading: loadingView,
+      Loading: progress => loadingView(loadingProgress),
       Error: errorView,
-      Ok: postsView,
+      Ok: resultsView,
       NotAsked: notAskedView
     })
   ]);
 }
 
 function errorView() {
-  return div([
-    'Error loading content',
-    button('.reload', "Reload")
-  ])
+  return div(['Error loading content', button('.reload', 'Reload')]);
 }
 
-function loadingView() {
-  return div('Loading...')
+function loadingView(progress: number) {
+  return div([
+    'Loading' +
+      Array(progress)
+        .fill('.')
+        .join('')
+  ]);
 }
 
 function notAskedView() {
-  return div([
-    'Search for something!'
-  ])
+  return div(['Search for something!']);
 }
 
-function postsView(posts) {
-  return div([
-    JSON.stringify(posts)
-  ])
+function resultsView(results: Result[]) {
+  if (results.length === 0) {
+    return div('No results found');
+  }
+
+  return div(results.map(post => div('.post', post.name + ' - ' + post.value)));
 }
 
 const drivers = {
   DOM: makeDOMDriver(document.body),
   RemoteData: makeRemoteDataDriver(),
   Time: timeDriver
-}
+};
 
 run(GithubSearch, drivers);
